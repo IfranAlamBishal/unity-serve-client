91b48dc73ebc04911f087c6a31ad17da9867c5be
Your NameIfranAlamBishal <you@example.comifranalam4089@gmail.com>
2024-05-14 04:19:16 +0600

    deleting post from manage my post page

--- src/Components/User/ManagePosts/MyPost.jsx
+++ src/Components/User/ManagePosts/MyPost.jsx
@@ -1,7 +1,37 @@
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const MyPost = ({ post }) => {
-    const { thumbnail, title, location, deadline } = post
+    const { _id, thumbnail, title, location, deadline } = post
+
+    const handleDelete = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You want to delete this post?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, I want delete."
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`https://unity-serve-server.vercel.app/volunteer_posts/${id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "You've successfully deleted the post. ",
+                                icon: "success"
+                            });
+                            window.location.reload()
+                        }
+                    })
+            }
+        });
+    }
     return (
         <div className="card sm:card-compact lg:card-side w-[320px] lg:w-full bg-blue-50 shadow-xl my-6 ">
             <figure><img src={thumbnail} alt="Property" className=" w-[320px] h-56" /></figure>
@@ -12,7 +42,7 @@ const MyPost = ({ post }) => {
 
                 <div className="card-actions justify-center lg:justify-end mt-5">
                     <Link className="btn btn-accent text-base font-semibold text-white">Update Post</Link>
-                    <Link className="btn btn-error text-base font-semibold text-white">Delete Post</Link>
+                    <Link onClick={() => handleDelete(_id)} className="btn btn-error text-base font-semibold text-white">Delete Post</Link>
                 </div>
             </div>
         </div>
