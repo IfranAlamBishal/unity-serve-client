a8006bcdced75dc86657f3b4e29524f4b0c5212f
Your NameIfranAlamBishal <you@example.comifranalam4089@gmail.com>
2024-07-02 18:31:41 +0600

    update : updated the reademe.md file

--- src/Components/NeedVolunteer/NeedVolunteer.jsx
+++ src/Components/NeedVolunteer/NeedVolunteer.jsx
@@ -6,6 +6,23 @@ const NeedVolunteer = () => {
 
     const posts = useLoaderData()
 
+    const categoryList = ['Animal Welfare', 'Education', 'Environment', 'Healthcare', 'Social Service', 'Other'];
+    const [selected, setSelected] = useState(null)
+    const [displayPost, setDisplayPost] = useState(posts)
+
+    const handleSelect = category => {
+        setSelected(category)
+
+        const filterPost = posts.filter(post => post.category == category)
+
+        if(filterPost){
+            setDisplayPost(filterPost)
+        }
+        else {
+            setDisplayPost(posts)
+        }
+    }
+
     const [spinner, setSpinner] = useState(true);
 
     useEffect(() => {
@@ -26,9 +43,29 @@ const NeedVolunteer = () => {
             <h2 className=" text-center text-4xl font-bold my-4">Need Volunteers</h2>
             <h4 className=" text-center text-2xl font-semibold my-4">Available Post: {posts.length}</h4>
             <hr className=" w-2/3 mx-auto py-4 border-gray-500" />
+
+            <div className="dropdown w-64 mb-5">
+                            <div tabIndex={0} role="button" className="btn btn-neutral ">
+                                <label className="label">
+                                    <span className="label-text text-white">Category</span>
+                                </label>
+                            </div>
+                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-white rounded-box w-52 text-base font-medium gap-1">
+                                {
+                                    categoryList.map(category => <li key={category} onClick={() => handleSelect(category)} className=" hover:bg-gray-200 cursor-pointer">{category}</li>)
+                                }
+                            </ul>
+                            <div className=" mt-2">
+                                {
+                                    selected && (
+                                        <h2>{selected}</h2>
+                                    )
+                                }
+                            </div>
+                        </div>
             <div  className="grid gap-8 grid-cols-1 md:grid-cols-2 w-full mx-auto">
                 {
-                    posts.map(post => <PostCard key={post._id} post={post}></PostCard>)
+                    displayPost.map(post => <PostCard key={post._id} post={post}></PostCard>)
                 }
             </div>
         </div>
